41164ef23aed1203e18369e336e4c40fb1916b3f
trippsc2 <tarpleyjim@gmail.com>
2020-06-24 09:07:51 -0400

    Reformatted Program class.

--- OpenTracker/Program.cs
+++ OpenTracker/Program.cs
@@ -9,15 +9,12 @@ namespace OpenTracker
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args) =>
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
         // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .LogToDebug()
-                .UseReactiveUI()
-                .With(new AvaloniaNativePlatformOptions { UseGpu = false });
+        public static AppBuilder BuildAvaloniaApp() =>
+            AppBuilder.Configure<App>().UsePlatformDetect().LogToDebug().UseReactiveUI()
+            .With(new AvaloniaNativePlatformOptions { UseGpu = false });
     }
 }
