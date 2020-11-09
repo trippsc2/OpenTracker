aeba9feb1837cc4d19e62863051bbd8c81ac781a
Tripp <trippsc2@github>
2020-11-09 09:32:15 -0500

    Upgraded Avalonia to 0.10 preview 6.

--- OpenTracker/Program.cs
+++ OpenTracker/Program.cs
@@ -13,7 +13,7 @@ namespace OpenTracker
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp() =>
-            AppBuilder.Configure<App>().UsePlatformDetect().LogToDebug().UseReactiveUI()
+            AppBuilder.Configure<App>().UsePlatformDetect().LogToTrace().UseReactiveUI()
             .With(new AvaloniaNativePlatformOptions { UseGpu = false });
     }
 }
