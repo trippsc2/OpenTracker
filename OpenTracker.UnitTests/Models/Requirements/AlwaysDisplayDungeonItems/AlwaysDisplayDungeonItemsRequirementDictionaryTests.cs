3f7375b294d6750ab15e3cd8961440a9e69c27f3
trippsc2 <trippsc2@github.com>
2021-05-11 14:22:42 -0400

    Added documentation and unit test.

---
+++ OpenTracker.UnitTests/Models/Requirements/AlwaysDisplayDungeonItems/AlwaysDisplayDungeonItemsRequirementDictionaryTests.cs
@@ -0,0 +1,50 @@
+using Autofac;
+using NSubstitute;
+using OpenTracker.Models.Requirements.AlwaysDisplayDungeonItems;
+using Xunit;
+
+namespace OpenTracker.UnitTests.Models.Requirements.AlwaysDisplayDungeonItems
+{
+    public class AlwaysDisplayDungeonItemsRequirementDictionaryTests
+    {
+        // ReSharper disable once CollectionNeverUpdated.Local
+        private readonly AlwaysDisplayDungeonItemsRequirementDictionary _sut;
+
+        public AlwaysDisplayDungeonItemsRequirementDictionaryTests()
+        {
+            static IAlwaysDisplayDungeonItemsRequirement Factory(bool expectedValue)
+            {
+                return Substitute.For<IAlwaysDisplayDungeonItemsRequirement>();
+            }
+
+            _sut = new AlwaysDisplayDungeonItemsRequirementDictionary(Factory);
+        }
+
+        [Fact]
+        public void Indexer_ShouldReturnTheSameInstance()
+        {
+            var requirement1 = _sut[false];
+            var requirement2 = _sut[false];
+            
+            Assert.Equal(requirement1, requirement2);
+        }
+
+        [Fact]
+        public void Indexer_ShouldReturnTheDifferentInstances()
+        {
+            var requirement1 = _sut[false];
+            var requirement2 = _sut[true];
+            
+            Assert.NotEqual(requirement1, requirement2);
+        }
+
+        [Fact]
+        public void AutofacTest()
+        {
+            using var scope = ContainerConfig.Configure().BeginLifetimeScope();
+            var sut = scope.Resolve<IAlwaysDisplayDungeonItemsRequirementDictionary>();
+            
+            Assert.NotNull(sut as AlwaysDisplayDungeonItemsRequirementDictionary);
+        }
+    }
+}
---
+++ OpenTracker.UnitTests/Utils/JsonConverterTests.cs
@@ -0,0 +1,28 @@
+using Autofac;
+using OpenTracker.Utils;
+using Xunit;
+
+namespace OpenTracker.UnitTests.Utils
+{
+    public class JsonConverterTests
+    {
+        [Fact]
+        public void AutofacTest()
+        {
+            using var scope = ContainerConfig.Configure().BeginLifetimeScope();
+            var sut = scope.Resolve<IJsonConverter>();
+            
+            Assert.NotNull(sut as JsonConverter);
+        }
+        
+        [Fact]
+        public void AutofacSingleInstanceTest()
+        {
+            using var scope = ContainerConfig.Configure().BeginLifetimeScope();
+            var value1 = scope.Resolve<IJsonConverter>();
+            var value2 = scope.Resolve<IJsonConverter>();
+            
+            Assert.Equal(value1, value2);
+        }
+    }
+}
